4118e2e3f2d8184f675d8e9cf04651e35fff43f8
Zeph <adarzeph@gmail.com>
2025-11-06 17:30:45 +0300

    Update IPS-H264.py

--- IPS-H264.py
+++ IPS-H264.py
@@ -24,7 +24,7 @@ logging.basicConfig(
 username = 'admin'
 password = 'kObliha12@'
 cameraIP = '192.168.2.13'
-cameraPort = 5000
+cameraPort = 80
 receiveAlarmDataIP = "192.168.2.13"
 receiveAlarmDataPort = 64073
 duration = 300
